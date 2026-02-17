f98cea3a5b5da5d91902c2ba06c6979c3141ca55
hn1f <hn1f@proton.me>
2026-02-17 23:16:17 +0100

    add warning
    in case it leaks out this early and people mindlessly install it :3

--- app/src/main/java/eu/hn1f/holoui/SystemUIApplication.kt
+++ app/src/main/java/eu/hn1f/holoui/SystemUIApplication.kt
@@ -1,6 +1,9 @@
 package eu.hn1f.holoui
 
+import android.app.AlertDialog
 import android.app.Application
+import android.content.DialogInterface
+import android.view.WindowManager
 
 class SystemUIApplication: Application() {
     var statusBarRunning = false
@@ -12,6 +15,17 @@ class SystemUIApplication: Application() {
     fun startServices() {
         setTheme(R.style.Theme_SystemUI)
         if(!statusBarRunning) {
+            val warning = AlertDialog.Builder(this)
+                .setTitle("Android System")
+                .setMessage("You're currently using HoloUI, an alternative SystemUI implementation"
+                    + " which is work-in-progress.\n\n"
+                    + "You may encounter bugs you wouldn't see with the original implementation.")
+                .setPositiveButton("OK", DialogInterface.OnClickListener {_, _ ->})
+                .create()
+
+            warning.window!!.setType(WindowManager.LayoutParams.TYPE_KEYGUARD_DIALOG)
+            warning.show()
+
             StatusBar(this).init()
             statusBarRunning = true
         }
