7ec9463c7140787cf665fb66431a73f685b3c4f9
hn1f <hn1f@proton.me>
2026-02-19 21:23:47 +0100

    unprivate notificationlister
    this probably will be moved into notificationshade anyway

--- app/src/main/java/eu/hn1f/holoui/NotificationListener.kt
+++ app/src/main/java/eu/hn1f/holoui/NotificationListener.kt
@@ -4,12 +4,11 @@ import android.service.notification.NotificationListenerService
 import android.service.notification.StatusBarNotification
 import android.util.Log
 
-private class NotificationListener: NotificationListenerService() {
+class NotificationListener: NotificationListenerService() {
     override fun onNotificationPosted(sbn: StatusBarNotification?) {
         super.onNotificationPosted(sbn)
         val text = sbn!!.notification.shortCriticalText;
         // val icon = sbn.notification.smallIcon
         Log.v("HoloUI", "new notification: $text")
     }
-
 }
