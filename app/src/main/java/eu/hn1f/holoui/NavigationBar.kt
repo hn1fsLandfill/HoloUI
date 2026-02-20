bf264f55f85fcaa9007eb703a159f06a187a0e66
hn1f <hn1f@proton.me>
2026-02-20 15:53:05 +0100

    make the navigation bar use a layout xml

--- app/src/main/java/eu/hn1f/holoui/NavigationBar.kt
+++ app/src/main/java/eu/hn1f/holoui/NavigationBar.kt
@@ -8,18 +8,18 @@ import android.os.Binder
 import android.view.Gravity
 import android.view.InsetsFrameProvider
 import android.view.KeyEvent
+import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.view.WindowInsets
 import android.view.WindowManager
 import android.widget.LinearLayout
-import eu.hn1f.holoui.widgets.NavigationKey
 
 class NavigationBar(val context: Context) {
     val windowManager = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
     var root: LinearLayout? = null
     val barHeight = context.resources.getDimensionPixelSize(R.dimen.navigationbar_height)
     var windowInsetsOwner = Binder();
+    var inflater = LayoutInflater.from(context)
 
     fun hide() {
         val animator = root!!.animate()
@@ -38,17 +38,17 @@ class NavigationBar(val context: Context) {
 
     // TODO: Landscape navigation bar
     fun add() {
-        root = LinearLayout(context)
-        root!!.orientation = LinearLayout.HORIZONTAL
-        root!!.gravity = Gravity.CENTER
+        root = inflater.inflate(R.layout.navigation_bar, null) as LinearLayout?
         val lp = WindowManager.LayoutParams(
             WindowManager.LayoutParams.MATCH_PARENT,
             barHeight,
             WindowManager.LayoutParams.TYPE_NAVIGATION_BAR,
             WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
-                    or WindowManager.LayoutParams.FLAG_SPLIT_TOUCH
-                    or WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS,
+                    or WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL
+                    or WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH
+                    or WindowManager.LayoutParams.FLAG_SLIPPERY,
             PixelFormat.TRANSLUCENT)
+        lp.privateFlags = WindowManager.LayoutParams.PRIVATE_FLAG_LAYOUT_SIZE_EXTENDED_BY_CUTOUT
         lp.token = Binder()
         lp.gravity = Gravity.BOTTOM
         lp.softInputMode = WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE
@@ -63,20 +63,8 @@ class NavigationBar(val context: Context) {
         windowManager.addView(root, lp)
     }
 
-    fun addKey(image: Int, key: Int) {
-        val b = NavigationKey(context, key)
-        b.layoutParams = ViewGroup.LayoutParams(
-            WindowManager.LayoutParams.WRAP_CONTENT,
-            WindowManager.LayoutParams.MATCH_PARENT
-        )
-        b.setImageResource(image)
-        root!!.addView(b)
-    }
-
     fun init() {
         add()
         root!!.setBackgroundColor(Color.BLACK)
-        addKey(R.drawable.ic_sysbar_home, KeyEvent.KEYCODE_HOME)
-        addKey(R.drawable.ic_sysbar_back, KeyEvent.KEYCODE_BACK)
     }
 }
--- app/src/main/java/eu/hn1f/holoui/widgets/NavigationKey.kt
+++ app/src/main/java/eu/hn1f/holoui/widgets/NavigationKey.kt
@@ -4,29 +4,48 @@ import android.content.Context
 import android.hardware.input.InputManagerGlobal
 import android.os.InputEventInjectionSync
 import android.os.SystemClock
+import android.util.AttributeSet
+import android.view.InputDevice
+import android.view.KeyCharacterMap
 import android.view.KeyEvent
 import android.view.MotionEvent
 import android.widget.ImageView
+import eu.hn1f.holoui.R
 
-class NavigationKey(context: Context?, val key: Int): ImageView(context) {
+class NavigationKey(context: Context?, attrs: AttributeSet?): ImageView(context, attrs) {
     var downTime = 0L
+    val highlight = resources.getDrawable(R.drawable.ic_sysbar_highlight)
+    val key = context!!.obtainStyledAttributes(attrs, R.styleable.NavigationKey)
 
     fun sendEvent(action: Int, key: Int, eventTime: Long) {
         InputManagerGlobal.getInstance().injectInputEvent(KeyEvent(
             downTime, eventTime,
             action,
             key,
-            0
+            0,
+            0,
+            KeyCharacterMap.VIRTUAL_KEYBOARD,
+            0,
+            KeyEvent.FLAG_FROM_SYSTEM or KeyEvent.FLAG_VIRTUAL_HARD_KEY,
+            InputDevice.SOURCE_KEYBOARD
         ), InputEventInjectionSync.NONE)
     }
 
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        // super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        setMeasuredDimension(highlight.intrinsicWidth, heightMeasureSpec)
+    }
+
+
     override fun onTouchEvent(event: MotionEvent?): Boolean {
         if(event?.action == MotionEvent.ACTION_UP) {
-            sendEvent(KeyEvent.ACTION_UP, key, SystemClock.uptimeMillis())
+            background = null
+            sendEvent(KeyEvent.ACTION_UP, key.getInt(R.styleable.NavigationKey_key, 3), SystemClock.uptimeMillis())
             return true
         } else if(event?.action == MotionEvent.ACTION_DOWN) {
+            background = highlight
             downTime = SystemClock.uptimeMillis()
-            sendEvent(KeyEvent.ACTION_DOWN, key, downTime)
+            sendEvent(KeyEvent.ACTION_DOWN, key.getInt(R.styleable.NavigationKey_key, 3), downTime)
             return true
         }
         return super.onTouchEvent(event)
