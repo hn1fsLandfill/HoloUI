a2fbdad9619ee71aafa8c9934433077617b117cf
hn1f <hn1f@proton.me>
2026-02-23 10:20:40 +0100

    remove leftovers from porting experiment

--- app/src/main/java/com/android/systemui/EventLogTags.java
+++
@@ -1,7 +0,0 @@
-package com.android.systemui;
-
-public class EventLogTags {
-    public static int SYSUI_STATUSBAR_TOUCH = 0;
-    public static int SYSUI_PANELHOLDER_TOUCH = 1;
-    public static void writeEvent(Object... any) {}
-}
