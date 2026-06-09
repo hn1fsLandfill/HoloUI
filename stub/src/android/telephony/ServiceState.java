ba672b88f53e19780b54102b00a9e026188ca5e5
hn1f <hn1f@proton.me>
2026-06-09 11:31:59 +0200

    oopsie

---
+++ stub/src/android/telephony/ServiceState.java
@@ -0,0 +1,45 @@
+package android.telephony;
+
+import android.annotation.SuppressLint;
+import android.os.Parcel;
+import android.os.Parcelable;
+
+@SuppressLint("ParcelCreator")
+public class ServiceState implements Parcelable {
+    public boolean isEmergencyOnly() {
+        return false;
+    }
+    public int getVoiceRegState() {
+        return 0;
+    }
+    public int getDataRegState() {
+        return 0;
+    }
+
+    /**
+     * Describe the kinds of special objects contained in this Parcelable
+     * instance's marshaled representation. For example, if the object will
+     * include a file descriptor in the output of {@link #writeToParcel(Parcel, int)},
+     * the return value of this method must include the
+     * {@link #CONTENTS_FILE_DESCRIPTOR} bit.
+     *
+     * @return a bitmask indicating the set of special object types marshaled
+     * by this Parcelable object instance.
+     */
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    /**
+     * Flatten this object in to a Parcel.
+     *
+     * @param dest  The Parcel in which the object should be written.
+     * @param flags Additional flags about how the object should be written.
+     *              May be 0 or {@link #PARCELABLE_WRITE_RETURN_VALUE}.
+     */
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+
+    }
+}
